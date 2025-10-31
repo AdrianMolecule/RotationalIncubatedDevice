234d278e6de42f0fc9511aa4a2d0a1564645aad3
adrian <tada@specyal.com>
2025-10-30 21:45:55 -0400

    good helpers

--- src/Helper.h
+++ src/Helper.h
@@ -1,9 +1,19 @@
 #pragma once
 #include <vector>
+#include <String.h>
 #include "Field.h"
 
 class Helper {
 public:
-    // Initialize the default model fields (factory defaults)
+    // Return the default set of fields
     static std::vector<Field> defaultFields();
+
+    // Generate the HTML menu/navigation
+    static String generateMenu();
+
+    // Generate HTML page header
+    static String htmlHeader(const String& title);
+
+    // Generate HTML page footer
+    static String htmlFooter();
 };

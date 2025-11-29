99d28f2b68ca14f582af9ab5d946cbf53a88745a
adrian <tada@specyal.com>
2025-11-29 13:58:27 -0500

    doc

--- src/pass.h
+++ src/pass.h
@@ -2,7 +2,7 @@
 #include <ArduinoJson.h>
 
 // on router go to Basic/lanSetup/dhcp reservation and click add reservation and choose 192.168.0.201 for bigOS 192.168.0.202 for TR and 192.168.0.203 for small OS
-const char* jsonString = "{\"ssid\":\"biohub\", \"password\":\"yourpass\"}";
+const char* jsonString = "{\"ssid\":\"ad5\", \"password\":\"XXX!\"}";
   JsonDocument doc;
 const char * getSsid(){
   DeserializationError error = deserializeJson(doc, jsonString);
